dd631ce3da07d6a3c276620278d3dd3acb0d4bf8
Chris Vita <chris.vita@gmail.com>
2017-06-16 17:29:11 -0700

    'Refactor email.js to store/handle all messages'

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Note that it takes a moment for Heroku's services to initially activate.
 ### Detailed description
 The app allows a user to paste in a valid Sony PlayStation store game URL and enter their email address. Upon confirmation, the game and user info are stored in a MongoDB collection. Then, once a day, the app's back-end will iterate through the database and scrape* the PlayStation store, comparing prices to determine if the game is on sale. If the game is on sale, the user will receive an email alert. If the game hasn't gone on sale for 18 weeks, the user's document in Mongo is deleted, and the user is invited to sign up again.
 
-*The PlayStation store doesn't have a public API, and loads quite slowly. So far, scraping Sony's site with the help of [HorsemanJS](https://github.com/johntitus/node-horseman) is working quite well!
+*The PlayStation store doesn't have a public API, and loads quite slowly. So far, scraping Sony's site with the help of [HorsemanJS](https://github.com/johntitus/node-horseman) is working well!
 
 #### Preview screenshot
 ![development screenshot as of 6-7-17](./demo-assets/screenshot-6-7-17.png "development screenshot as of 6-7-17")
--- api/email.js
+++ api/email.js
@@ -1,16 +1,15 @@
 const nodemailer = require('nodemailer');
-
-var emailPassword = process.env.emailPassword || require('../local-dev-creds').emailPassword;
+const crypt = require('encrypt');
 
 const transport = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
         user: 'game.price.tracker@gmail.com',
-        pass: emailPassword
+        pass: process.env.emailPassword || require('../local-dev-creds').emailPassword
     }
 });
 
-module.exports = function sendEmail(to, subject, message) {
+function sendEmail(to, subject, message) {
     const mailOptions = {
         from: 'game.price.tracker@gmail.com',
         to: to,
@@ -25,4 +24,44 @@ module.exports = function sendEmail(to, subject, message) {
             console.log('Message sent!');
         }
     });
-};
+}
+
+// Todo: Consider encrypting the mongoDoc._id
+function sendConfirmationEmail(mongoDoc, uri) {
+    var unsubscribeUrl = 'https://' + uri + '/#/unsubscribe?id=' + crypt.encrypt(mongoDoc._id);
+    unsubscribeUrl += '/?user=' + crypt.encrypt(mongoDoc.userEmail);
+
+    var userEmail = mongoDoc.userEmail;
+    var subject = mongoDoc.game + ' is now being tracked';
+    var message = (
+        'Game Price Tracker is now tracking the price of <i>' + mongoDoc.game + '</i>. ' +
+        'If it drops below ' + mongoDoc.price + ' before ' + mongoDoc.expiration + ', ' +
+        'you will be messaged again at this email address.<br><br>' +
+        'All done? <a href=' + unsubscribeUrl + '>Unsubscribe</a>'
+    );
+    sendEmail(userEmail, subject, message);
+}
+
+function sendSalePriceEmail(mongoDoc, newScrape) {
+    var userEmail = mongoDoc.userEmail;
+    var subject = mongoDoc.game + ' is on sale';
+    var message = (
+        mongoDoc.game + ' is currently on sale for ' + newScrape.price + '.' +
+        ' Check it out <a href=' + mongoDoc.gameUrl + '>here</a>'
+    );
+    sendEmail(userEmail, subject, message);
+}
+
+function sendRemovingPriceAlertEmail(mongoDoc) {
+    var userEmail = mongoDoc.userEmail;
+    var subject = 'Removing Game Price Tracker alert for ' + mongoDoc.game;
+    var message = (
+        mongoDoc.game + ' has not gone on sale for 18 weeks. We are removing this price alert. ' +
+        'Feel free to visit Game Price Tracker to sign up for another 18 week period.'
+    );
+    sendEmail(userEmail, subject, message);
+}
+
+
+const email = { sendConfirmationEmail, sendSalePriceEmail, sendRemovingPriceAlertEmail };
+module.exports = email;
