779ab07a50e7a1e25e5bce69f09c145cd3bb1937
Chris Vita <chris.vita@gmail.com>
2017-07-05 17:18:00 -0700

    'Refactor scrape.js to reflect changes in mongo schema'

--- api/scrape.js
+++ api/scrape.js
@@ -1,10 +1,11 @@
 const Horseman = require('node-horseman');
 
-module.exports = function scrapeSony(gameUrl) {
+// Does not allow arrow functions
+module.exports = function scrapeSony(url) {
     return new Promise(function (resolve, reject) {
         console.log('Running scrapeSony()...');
 
-        if (gameUrl.indexOf('store.playstation.com') === -1) {
+        if (url.indexOf('store.playstation.com') === -1) {
             console.log('Error, must be a Sony PlayStation store url');
             return;
         }
@@ -16,23 +17,28 @@ module.exports = function scrapeSony(gameUrl) {
 
         horseman
             .userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0')
-            .open(gameUrl)
-            .waitForSelector('.buyPrice')
+            .open(url)
+            .waitForSelector('.buyPrice', '.productTitle')
             .evaluate(function () {
-                const strikePrice = $('.strikePrice:not(li) .price').text();
-                const onSale = strikePrice === "" ? false : true;
+                var onSale = false;
+                var strikePrice = $('.strikePrice:not(li) .price').text();
+                if (strikePrice !== '') {
+                    onSale = true;
+                    strikePrice = parseFloat(strikePrice.slice(1));
+                }
 
                 return {
-                    "game": $('.productTitle').text(),
-                    "gameImage": $('.productThumbImg img').attr('src'),
-                    "price": $('.buyPrice .price').text(),
-                    "priceInt": Math.ceil($('.buyPrice .price').text().slice(1)),
-                    "strikePrice": strikePrice,
-                    "onSale": onSale
-                }
+                    _id: $('.productTitle').text(),
+                    price: parseFloat($('.buyPrice .price').text().slice(1)),
+                    onSale: onSale,
+                    strikePrice: strikePrice,
+                    lastUpdated: new Date().toDateString(),
+                    image: $('.productThumbImg img').attr('src'),
+                };
             })
             .then(function (result) {
                 horseman.close();
+                result.url = url;
                 resolve(result);
             })
             .catch(function (err) {
