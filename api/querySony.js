04c57d0257452270a092ab60f5cbf91db7789221
Chris Vita <chris.vita@gmail.com>
2017-07-14 16:18:18 -0700

    'Add more conditions to normalize info returned from Sony'

--- api/querySony.js
+++ api/querySony.js
@@ -24,27 +24,43 @@ module.exports = function querySony(gameCode) {
                     let saleInfo = game.default_sku.rewards[0];
 
                     salePrice = parseFloat(saleInfo.display_price.slice(1));
-                    discount = parseInt(saleInfo.discount);
+                    discount = parseInt(saleInfo.discount, 10);
                     strikePrice = normalPrice;
                 }
 
-                resolve({
+                const info = {
                     _id: game.title_name,
-                    url: 'https://store.playstation.com/#!/en-us/games/' + game.id,
+                    url: 'https://store.playstation.com/#!/en-us/games/cid=' + game.id,
                     price: normalPrice || salePrice,
+                    strikePrice: strikePrice,
                     onSale: onSale,
                     discount: discount,
                     lastDateOnSale: null, // Todo: track last known sale
                     lastUpdated: today,
                     image: options.uri + '/image?_version=00_09_000&platform=chihiro&w=400&h=400',
+                    media: { screenshots: [], videos: [] }
+                };
+
+                info.details = {
+                    description: game.long_desc.slice(0, game.long_desc.indexOf('<br>')),
                     releaseDate: game.release_date,
                     gameDev: game.provider_name,
-                    media: {
-                        screenshots: game.mediaList.screenshots.slice(0, 3).map(pic => pic.url),
-                        videos: game.promomedia.slice(0, 2).map(video => video.url),
-                        description: game.long_desc.slice(0, game.long_desc.indexOf('<br>'))
+                    platforms: game.playable_platform.map(system => system.slice(0, system.indexOf('™'))),
+                    starRating: parseFloat(game.star_rating.score),
+                    esrbRating: game.content_rating.url
+                };
+
+                if (game.hasOwnProperty('mediaList')) {
+                    if (game.mediaList.hasOwnProperty('screenshots')) {
+                        info.media.screenshots = game.mediaList.screenshots.slice(0, 3).map(pic => pic.url);
                     }
-                });
+                }
+
+                if (game.promomedia.hasOwnProperty('url')) {
+                    info.media.videos = game.promomedia.slice(0, 2).map(video => video.url);
+                }
+
+                resolve(info);
             })
             .catch(err => {
                 reject(err);
