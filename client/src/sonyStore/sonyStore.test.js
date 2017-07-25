c094a1fc2c79e6bd492c15f062b23489478c43ec
Chris Vita <chris.vita@gmail.com>
2017-07-25 14:02:33 -0700

    'Replace mock of sonyStore using fetch-mock'

--- client/src/sonyStore/sonyStore.test.js
+++ client/src/sonyStore/sonyStore.test.js
@@ -1,5 +1,6 @@
-jest.mock('./sonyStore');
 import sonyStore from './sonyStore';
+const fetchMock = require('fetch-mock');
+const fs = require('fs');
 
 
 const singleResultSearchEx = {
@@ -53,28 +54,45 @@ const multipleResultSearchEx = {
 
 
 it('returns expected values when searching sony store by game id', () => {
+  fetchMock.get(
+    'https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/19/UP0082-CUSA05532_00-FFXIIGAMEPS400NA',
+    fs.readFileSync('./src/sonyStore/__mockData__/sonyStoreApiSearchById.json', 'utf8')
+  );
   expect.assertions(2);
-  return sonyStore.findGameById('UP0002-CUSA07402_00-CRASHNSANETRLOGY')
+  return sonyStore.findGameById('UP0082-CUSA05532_00-FFXIIGAMEPS400NA')
     .then(resp => {
       expect(resp).toBeDefined();
       expect(resp).toEqual(singleResultSearchEx);
+      fetchMock.restore();
     });
 });
 
+
 it('returns expected values when searching sony store by game title', () => {
+  fetchMock.get(
+    'https://store.playstation.com/store/api/chihiro/00_09_000/tumbler/US/en/19/final%20fantasy%20XII?suggested_size=5&mode=game&mode=film&mode=tv&mode=live_event',
+    fs.readFileSync('./src/sonyStore/__mockData__/sonyStoreApiSearchByTitle.json', 'utf8')
+  );
   expect.assertions(2);
-  return sonyStore.findGameByTitle('crash bandicoot')
+  return sonyStore.findGameByTitle('final fantasy XII')
     .then(resp => {
       expect(resp).toBeDefined();
       expect(resp[0]).toEqual(multipleResultSearchEx);
+      fetchMock.restore();
     });
 });
 
+
 it('returns expected values when searching sony store for new games', () => {
+  fetchMock.get(
+    'https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/19/STORE-MSF77008-NEWGAMESGRID?size=25',
+    fs.readFileSync('./src/sonyStore/__mockData__/sonyStoreApiFindNewGames.json', 'utf8')
+  );
   expect.assertions(2);
   return sonyStore.findNewGames(25)
     .then(resp => {
       expect(resp).toBeDefined();
       expect(resp[0]).toEqual(multipleResultSearchEx);
+      fetchMock.restore();
     });
 });
