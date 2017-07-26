1e88f77ffdeca43f5620e08e0a4fb6324ab34af6
Chris Vita <chris.vita@gmail.com>
2017-07-26 11:21:12 -0700

    'Develop tests for fetch calls to db'

--- client/src/Client.js
+++ client/src/Client.js
@@ -1,76 +1,86 @@
 function findAllGames() {
     return new Promise((resolve, reject) => {
-        const request = new Request('/games/find/all');
-        fetch(request, { method: 'GET' })
+        fetch('/games/find/all', { method: 'GET' })
             .then(response => handleResponse(response, resolve, reject));
     });
 }
 
 function findOneGame(url) {
     return new Promise((resolve, reject) => {
-        const request = new Request(`/games/find/one/?url=${encodeURIComponent(url)}`);
-        fetch(request, { method: 'GET' })
+        fetch(`/games/find/one/?url=${encodeURIComponent(url)}`, { method: 'GET' })
             .then(response => handleResponse(response, resolve, reject));
     });
 }
 
 function addOrUpdateGame(gameInfo) {
     return new Promise((resolve, reject) => {
-        const request = new Request('/games/add', {
-            headers: new Headers({ 'Content-Type': 'application/json' })
-        });
-        fetch(request, { method: 'POST', body: JSON.stringify(gameInfo) })
+        const init = {
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+            method: 'POST',
+            body: JSON.stringify(gameInfo)
+        };
+        fetch('/games/add', init)
             .then(response => handleResponse(response, resolve, reject));
     });
 }
 
 function createPriceAlert(priceAlertInfo) {
+    console.log(priceAlertInfo);
     return new Promise((resolve, reject) => {
-        const request = new Request('/priceAlerts/add', {
-            headers: new Headers({ 'Content-Type': 'application/json' })
-        });
-        fetch(request, { method: 'POST', body: JSON.stringify(priceAlertInfo) })
+        const init = {
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+            method: 'POST',
+            body: JSON.stringify(priceAlertInfo)
+        };
+        fetch('/priceAlerts/add', init)
             .then(response => handleResponse(response, resolve, reject));
     });
 }
 
 function findOnePriceAlert(id) {
     return new Promise((resolve, reject) => {
-        console.log(id);
-        const request = new Request('/priceAlerts/find/one', {
-            headers: new Headers({ 'Content-Type': 'application/json' })
-        });
-        fetch(request, { method: 'POST', body: JSON.stringify({ id: id }) })
+        const init = {
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+            method: 'POST',
+            body: JSON.stringify({ id: id })
+        };
+        fetch('/priceAlerts/find/one', init)
             .then(response => handleResponse(response, resolve, reject));
     });
 }
 
 function deletePriceAlert(userInfo) {
     return new Promise((resolve, reject) => {
-        const request = new Request('/priceAlerts/delete', {
-            headers: new Headers({ 'Content-Type': 'application/json' })
-        });
-        fetch(request, { method: 'DELETE', body: JSON.stringify(userInfo) })
+        const init = {
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+            method: 'DELETE',
+            body: JSON.stringify(userInfo)
+        };
+        fetch('/priceAlerts/delete', init)
             .then(response => handleResponse(response, resolve, reject));
     });
 }
 
 function checkBlacklist(userEmail) {
     return new Promise((resolve, reject) => {
-        const request = new Request('/blacklist/find/one', {
-            headers: new Headers({ 'Content-Type': 'application/json' })
-        });
-        fetch(request, { method: 'POST', body: JSON.stringify({ userEmail: userEmail }) })
+        const init = {
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+            method: 'POST',
+            body: JSON.stringify({ userEmail: userEmail })
+        };
+        fetch('/blacklist/find/one', init)
             .then(response => handleResponse(response, resolve, reject));
     });
 }
 
 function addToBlacklist(userEmail) {
     return new Promise((resolve, reject) => {
-        const request = new Request('/blacklist/add', {
-            headers: new Headers({ 'Content-Type': 'application/json' })
-        });
-        fetch(request, { method: 'PUT', body: JSON.stringify({ userEmail: userEmail }) })
+        const init = {
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+            method: 'PUT',
+            body: JSON.stringify({ userEmail: userEmail })
+        };
+        fetch('/blacklist/add', init)
             .then(response => handleResponse(response, resolve, reject));
     });
 }
