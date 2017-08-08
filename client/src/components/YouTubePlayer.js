4b56e5e0c5950638f7ff5700c0e1e09d63bc9dc7
Chris Vita <chris.vita@gmail.com>
2017-08-08 09:16:40 -0700

    Allow full screen

--- client/src/components/YouTubePlayer.js
+++ client/src/components/YouTubePlayer.js
@@ -4,26 +4,27 @@ import './YouTubePlayer.css';
 
 class YouTubePlayer extends Component {
   componentDidMount() {
-    if (this.props.activeGame) {
+    if (this.props) {
       this.props.searchVideo(
-        this.props.activeGame.title + ' ' + this.props.activeGame.details.platforms[0]
+        this.props.title + ' ' + this.props.details.platforms[0].slice(0, 3) // slice to eliminate '™' from 'PS3'/'PS4'
       );
     }
   }
   render() {
-    const videoId = this.props.activeGame.videos;
+    const videoId = this.props.videos;
     const src = 'https://www.youtube.com/embed/' + videoId + '?enablejsapi=1';
 
     return (
       <div>
-        {this.props.activeGame && this.props.activeGame.videos &&
+        {this.props && this.props.videos &&
           <div className='videoWrapper'>
             <iframe
               id='player'
-              title={this.props.activeGame.title}
+              title={this.props.title}
               type='text/html'
               src={src}
               frameBorder='0'
+              allowFullScreen
             />
           </div>}
       </div>
