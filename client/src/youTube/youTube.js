a4e2ff36aa42b38bb1ef4c373b9397008601f4b1
Chris Vita <chris.vita@gmail.com>
2017-08-08 09:16:07 -0700

    Change YouTube key, and relocate to project configVars

--- client/src/youTube/youTube.js
+++ client/src/youTube/youTube.js
@@ -1,8 +1,7 @@
-function searchVideo(keywords) {
+function searchVideo(keywords, maxResults = 3) {
   return new Promise((resolve, reject) => {
-    console.log('ABOUT TO SEARCH YOUTUBE for', keywords);
-    const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=${keywords}&regionCode=US&type=video&videoCategoryId=20&videoEmbedable=true`;
-    const key = '&key=AIzaSyAxXnGEhkkZmEh7zfugJpAsJ7kpSU4GbDc';
+    const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=${maxResults}&q=${keywords}&regionCode=US&type=video&videoCategoryId=20&videoEmbedable=true&key=`;
+    const key = process.env.youTubeKey || require('../local-dev-creds').youTubeKey;
     const request = apiUrl + key;
     fetch(request, { method: 'GET' })
       .then(resp => resp.json())
