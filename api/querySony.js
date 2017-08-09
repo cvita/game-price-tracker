cd62342b222d31a91f4a034170671eb4a0453e74
Chris Vita <chris.vita@gmail.com>
2017-08-09 15:45:46 -0700

    Rewrite to closely match methods in `sonyStore.js`

--- api/querySony.js
+++ api/querySony.js
@@ -1,70 +1,82 @@
 const rp = require('request-promise');
 
 
-module.exports = function querySony(gameCode) {
+module.exports = function querySony(gameId) {
     return new Promise((resolve, reject) => {
-        console.log(`Now running querySony(${gameCode})`);
-
+        if (!validateGameId(gameId)) {
+            reject(new Error('invalid game ID'));
+            return;
+        }
         const options = {
-            uri: 'https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/19/' + gameCode,
+            uri: 'https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/19/' + gameId,
             headers: { 'User-Agent': 'Request-Promise' },
             json: true
         };
-
         rp(options)
-            .then(game => {
-                const today = new Date(new Date().toDateString()).getTime();
-                const normalPrice = parseFloat(game.skus[0].display_price.slice(1));
-                const onSale = game.default_sku.rewards.length > 0;
-                var salePrice = null;
-                var discount = null;
-                var strikePrice = null;
-
-                if (onSale) {
-                    let saleInfo = game.default_sku.rewards[0];
-
-                    salePrice = parseFloat(saleInfo.display_price.slice(1));
-                    discount = parseInt(saleInfo.discount, 10);
-                    strikePrice = normalPrice;
-                }
-
-                const info = {
-                    _id: game.id,
-                    title: game.title_name,
-                    url: 'https://store.playstation.com/#!/en-us/games/cid=' + game.id,
-                    price: normalPrice || salePrice,
-                    strikePrice: strikePrice,
-                    onSale: onSale,
-                    discount: discount,
-                    lastDateOnSale: null, // Todo: track last known sale
-                    lastUpdated: today,
-                    image: options.uri + '/image?_version=00_09_000&platform=chihiro&w=400&h=400',
-                    media: { screenshots: [], videos: [] }
-                };
-
-                info.details = {
-                    description: game.long_desc.slice(0, game.long_desc.indexOf('<br>')),
-                    releaseDate: game.release_date,
-                    gameDev: game.provider_name,
-                    platforms: game.playable_platform.map(system => system.slice(0, system.indexOf('™'))),
-                    starRating: parseFloat(game.star_rating.score),
-                    esrbRating: game.content_rating.url
-                };
-
-                if (game.hasOwnProperty('mediaList')) {
-                    if (game.mediaList.hasOwnProperty('screenshots')) {
-                        info.media.screenshots = game.mediaList.screenshots.slice(0, 3).map(pic => pic.url);
-                    }
-                }
-
-                if (game.promomedia.hasOwnProperty('url')) {
-                    info.media.videos = game.promomedia.slice(0, 2).map(video => video.url);
-                }
-
-                resolve(info);
-            })
-            .catch(err => {
-                reject(err);
-            });
+            .then(resp => processSingleResultGameInfo(resp))
+            .then(resp => resolve(resp))
+            .catch(err => reject(err));
     });
-};
+}
+
+
+function validateGameId(gameId) {
+    const idValidation = /UP\d{4}-\w{9}_00-\w{16}/g;
+    return idValidation.test(gameId.toString());
+}
+
+function processSingleResultGameInfo(game) {
+    return new Promise(resolve => {
+        const info = parseBasicGameInfo(game);
+
+        info.details = {
+            description: game.long_desc,
+            releaseDate: game.release_date,
+            gameDev: game.provider_name,
+            platforms: game.playable_platform,
+            starRating: parseFloat(game.star_rating.score),
+            esrbRating: game.content_rating.url
+        };
+
+        try {
+            info.screenshots = game.mediaList.screenshots.slice(0, 3).map(pic => pic.url);
+        } catch (e) {
+            info.screenshots = [];
+        }
+
+        resolve(info);
+    });
+}
+
+function parseBasicGameInfo(game, imageSize = 400) {
+    const normalPrice = game.default_sku.display_price !== 'Free' ?
+        parseFloat(game.default_sku.display_price.slice(1)) :
+        parseFloat('0.00').toFixed(2);
+
+    const info = {
+        _id: game.id,
+        title: game.name,
+        url: `https://store.playstation.com/#!/en-us/games/cid=${game.id}`,
+        price: normalPrice,
+        strikePrice: null,
+        onSale: game.default_sku.rewards.length > 0,
+        discount: null,
+        psPlusPrice: null,
+        lastUpdated: new Date(new Date().toDateString()).getTime(),
+        image: `https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/19/${game.id}/image?w=${imageSize}&h=${imageSize}`,
+        details: { platforms: game.playable_platform }
+    };
+
+    if (info.onSale) {
+        let saleInfo = game.default_sku.rewards[0];
+        if (!saleInfo.isPlus) {
+            info.price = saleInfo.display_price !== 'Free' ? parseFloat(saleInfo.display_price.slice(1)) : parseFloat('0.00').toFixed(2);
+            info.strikePrice = normalPrice;
+        } else {
+            info.psPlusPrice = saleInfo.display_price !== 'Free' ? parseFloat(saleInfo.display_price.slice(1)) : parseFloat('0.00').toFixed(2);
+        }
+        info.discount = parseInt(saleInfo.discount, 10);
+    }
+
+    return info;
+}
