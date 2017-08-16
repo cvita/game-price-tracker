8e02c7d0b27666c77d8695ec344d233db0f94fff
Chris Vita <chris.vita@gmail.com>
2017-08-16 13:24:17 -0700

    Add more specific assertions on schedule.test.js

--- api/__tests__/schedule.test.js
+++ api/__tests__/schedule.test.js
@@ -16,7 +16,7 @@ const examplePriceAlerts = [
     "userEmail": "gamePriceTracker.user@gmail.com",
     "onBlacklist": false,
     "game_id": "UP9000-CUSA00552_00-THELASTOFUS00000",
-    "price": 19.99,
+    "price": 0.01, // Set to ensure onSale === false
     "dateAdded": 1502434800000,
     "expiration": 1513321200000,
     "gameTitle": "The Last Of Us™ Remastered "
@@ -25,7 +25,7 @@ const examplePriceAlerts = [
     "userEmail": "gamePriceTracker.user@gmail.com",
     "onBlacklist": false,
     "game_id": "UP1001-CUSA03979_00-BIOSHOCKCOLLECTN",
-    "price": 159.99, // Set to trigger a sale event
+    "price": 159.99, // Set to trigger a sale event. onSale === true
     "dateAdded": 1502434800000,
     "expiration": 1513321200000,
     "gameTitle": "BioShock: The Collection"
@@ -47,10 +47,24 @@ describe('schedule.js', () => {
     return updateInfoAndInformUsers().then(status => {
       expect(status).toBeInstanceOf(Object);
       requiredKeys.forEach(key => expect(status).toHaveProperty(key));
-      expect(typeof status.started).toBe('number');
-      expect(typeof status.elapsed).toBe('number');
-      expect(Array.isArray(status.expiredAlerts)).toBe(true);
-      expect(Array.isArray(status.currentGames)).toBe(true);
+      checkTypesOnStatusObject(status);
+
+      const expectedExpiredAlerts = [
+        { title: examplePriceAlerts[0].gameTitle, userEmail: examplePriceAlerts[0].userEmail }
+      ];
+      const expectedCurrentGames = [
+        { onSale: false, title: examplePriceAlerts[1].gameTitle },
+        { onSale: true, title: examplePriceAlerts[2].gameTitle }
+      ];
+      expect(status.expiredAlerts).toEqual(expect.arrayContaining(expectedExpiredAlerts));
+      expect(status.currentGames).toEqual(expect.arrayContaining(expectedCurrentGames));
     });
   });
 });
+
+function checkTypesOnStatusObject(status) {
+  expect(typeof status.started).toBe('number');
+  expect(typeof status.elapsed).toBe('number');
+  expect(Array.isArray(status.expiredAlerts)).toBe(true);
+  expect(Array.isArray(status.currentGames)).toBe(true);
+}
