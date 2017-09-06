0f9c65408f292adc6ecc65242aa1e346c5e81286
Chris Vita <chris.vita@gmail.com>
2017-09-06 13:53:51 -0700

    Add serviceWorker()

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 import GamePriceTracker from './GamePriceTracker';
+import './index.css';
 
 // Service worker disabled for development...
-// import registerServiceWorker from './registerServiceWorker';
-
-import './index.css';
+import registerServiceWorker from './registerServiceWorker';
 
 
 const rootEl = document.getElementById('root');
@@ -25,4 +24,4 @@ if (module.hot) {
     });
 }
 
-// registerServiceWorker();
+registerServiceWorker();
