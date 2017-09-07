8e3383d34e4a480fd998765714f69cabd099be78
Chris Vita <chris.vita@gmail.com>
2017-09-07 09:45:05 -0700

    shhss

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const expressStatic = require('express').static;
 
 app.set('port', (process.env.PORT || 3001));
 
+app.get('*', function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+        res.redirect('https://' + req.get('host') + req.url);
+    } else {
+        next();
+    }
+});
+
 app.get('*.js', function (req, res, next) {
     console.log('REQ.URL', req.url);
     req.url = req.url + '.gz';
@@ -20,7 +28,6 @@ app.get('*.css', function (req, res, next) {
 });
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(requireHTTPS);
     app.use(expressStatic('client/build'));
     app.get('*', function (req, res) {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
@@ -30,11 +37,3 @@ if (process.env.NODE_ENV === 'production') {
 app.listen(app.get('port'), function () {
     console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
 });
-
-
-function requireHTTPS(req, res, next) {
-    if (req.headers['X-Forwarded-Proto'] !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    return next();
-}
