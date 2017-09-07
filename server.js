7fb03c13b0049907e3ba006f54258a7154e86798
Chris Vita <chris.vita@gmail.com>
2017-09-07 10:42:23 -0700

    Add `build` folder for test deployment

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('*', function (req, res, next) {
 
 app.get('*.js*', function (req, res, next) {
     console.log('REQ.URL', req.url);
-    if (req.url !== 'service-worker.js') {
+    if (req.url !== '/service-worker.js') {
         req.url = req.url + '.gz';
         res.set('Content-Encoding', 'gzip');
     }
@@ -25,7 +25,7 @@ app.get('*.js*', function (req, res, next) {
 app.get('*.css*', function (req, res, next) {
     req.url = req.url + '.gz';
     res.set('Content-Encoding', 'gzip');
-    res.set('Content-Type', 'text/css');
+    res.set('Content-Type', 'text/css'); // try setting Content-type for service-worker.js
     next();
 });
 
