3da27504bf9092a8a95b874918903391438d03a3
Chris Vita <chris.vita@gmail.com>
2017-09-08 16:25:11 -0700

    Add `build` folder for test deployment

--- server.js
+++ server.js
@@ -5,51 +5,27 @@ const expressStatic = require('express').static;
 
 app.set('port', (process.env.PORT || 3001));
 
-app.use('*', function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') { // Specific to Heroku
-        res.redirect('https://' + req.get('host') + req.url);
-    } else {
-        next();
-    }
-});
-
-// app.get('*.js*', function (req, res, next) {
-//     if (req.url !== '/service-worker.js' && req.url !== '/manifest.json') {
-//         req.url = req.url + '.gz';
-//         res.set('Content-Encoding', 'gzip');
-//     }
-//     next();
-// });
-
-
-app.get('*', function (req, res, next) {
-    if (req.url.indexOf('main.') !== -1) {
-        req.url = req.url + '.gz';
-        res.set('Content-Encoding', 'gzip');
-        
-        if (req.url.indexOf('.css') !== -1) {
-            res.set('Content-Type', 'text/css');
-        }
-    }
-
-
-
-
-    // if (req.url !== '/service-worker.js' && req.url !== '/manifest.json') {
-    //     req.url = req.url + '.gz';
-    //     res.set('Content-Encoding', 'gzip');
-    // }
-    next();
-});
-
-// app.get('*.css*', function (req, res, next) {
-//     req.url = req.url + '.gz';
-//     res.set('Content-Encoding', 'gzip');
-//     res.set('Content-Type', 'text/css'); // try setting Content-type for service-worker.js
-//     next();
-// });
-
 if (process.env.NODE_ENV === 'production') {
+    app.use('*', function (req, res, next) {
+        if (req.headers['x-forwarded-proto'] !== 'https') { // Specific to Heroku
+            res.redirect('https://' + req.get('host') + req.url);
+        } else {
+            next();
+        }
+    });
+
+    app.get('*', function (req, res, next) {
+        if (req.url.indexOf('main.') !== -1) {
+            req.url = req.url + '.gz';
+            res.set('Content-Encoding', 'gzip');
+
+            if (req.url.indexOf('.css') !== -1) {
+                res.set('Content-Type', 'text/css');
+            }
+        }
+        next();
+    });
+
     app.use(expressStatic('client/build'));
     app.get('*', function (req, res) {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
