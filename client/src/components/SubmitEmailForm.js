ed9c8083c9aa72d5ce5b95c8dadd0b20c885c3c6
Chris Vita <chris.vita@gmail.com>
2017-09-18 09:46:41 -0700

    Fix regEx for email validation
    Replaced regEx with more complete test.

--- client/src/components/SubmitEmailForm.js
+++ client/src/components/SubmitEmailForm.js
@@ -21,7 +21,7 @@ class SubmitEmailForm extends Component {
         }
     }
     render() {
-        const emailRegExp = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        const emailRegExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         const emailIsValid = emailRegExp.test(this.state.emailInput);
         const color = emailIsValid ?
             'success' :
