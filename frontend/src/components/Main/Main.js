078c8861e3018cdd0596844616f0a860bdbc4f6d
Chris Vita <chris.vita@gmail.com>
2017-12-19 14:54:40 -0800

    Add sticky footer

--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { Route, Switch, Link } from 'react-router-dom';
 
 import gamepad from '../../assets/gamepad.svg';
-import { Container, Row, Col } from 'reactstrap';
+import { Container } from 'reactstrap';
 import LoadingBar from 'react-redux-loading-bar';
 import ErrorAlert from './ErrorAlert';
+import Navigation from './Navigation';
 import Footer from './Footer';
 import './Main.css';
 
@@ -17,32 +18,23 @@ import NotFound from './NotFound';
 
 const Main = props => (
     <div className='mainBody'>
+        <header className='mainHeader'>
+            <Navigation />
+            <LoadingBar className='loading' />
+            <ErrorAlert errors={props.errors} />
+        </header>
 
-        <div className='mainContent'>
-            <header>
-                <LoadingBar className='loading' />
-                <Link className='siteHeaderContainer' to='/' onClick={props.resetActiveGame}>
-                    <h1 className='siteHeader'>Game Price Tracker</h1>
-                    <img className='gamepad' src={gamepad} alt='gamepad' />
-                </Link>
-                <ErrorAlert errors={props.errors} />
-            </header>
-            <main>
-                <Container>
-                    <Row>
-                        <Col>
-                            <Switch>
-                                <Route path='/' exact={true} render={() => <Home {...props} />} />
-                                <Route path='/search' render={() => <SearchResults {...props} />} />
-                                <Route path='/games' render={() => <PriceAlert {...props} />} />
-                                <Route path='/manage' render={() => <Unsubscribe {...props} />} />
-                                <Route component={NotFound} />
-                            </Switch>
-                        </Col>
-                    </Row>
-                </Container>
-            </main>
-        </div>
+        <main className='mainContent'>
+            <Container>
+                <Switch>
+                    <Route path='/' exact={true} render={() => <Home {...props} />} />
+                    <Route path='/search' render={() => <SearchResults {...props} />} />
+                    <Route path='/games' render={() => <PriceAlert {...props} />} />
+                    <Route path='/manage' render={() => <Unsubscribe {...props} />} />
+                    <Route component={NotFound} />
+                </Switch>
+            </Container>
+        </main>
 
         <footer className='mainFooter'>
             <Footer />
