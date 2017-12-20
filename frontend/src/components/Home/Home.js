a7bf78db61a6378227261c648e77e0169becd61d
Chris Vita <chris.vita@gmail.com>
2017-12-19 16:03:01 -0800

    Fix small UI style bugs (#15)
    * Fix broken paths to config vars
    
    * Add sticky footer
    
    * Add navbar
    
    * Cut `Currently tracking` list on `<Home />`
    
    Avoid redundant game listings.
    
    * Fix style bug on `<Game />` subcomponent
    
    `<Game />` should center on small screens.
    
    * Fix style bug on `<GameDetails />`
    
    `<Description />` should fill entire row on small screens.
    
    * Cut unused vars
    
    * Cut incompatible assertions
    
    Manual acceptance test successful. Long term fix in #14.

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Jumbotron } from 'reactstrap';
+import { Row, Col, Jumbotron } from 'reactstrap';
 import TitleSearch from './TitleSearch';
 import GamesGrid from '../helper/GamesGrid';
 
@@ -7,33 +7,26 @@ import GamesGrid from '../helper/GamesGrid';
 class Home extends Component {
   componentDidMount() {
     this.props.resetActiveGame();
-    if (this.props.allGames.length === 0) {
-      this.props.fetchAllGamesInDb(10);
-    }
     if (this.props.popularGames.length === 0) {
       this.props.findPopularGames(20);
     }
   }
   render() {
-
     return (
       <div>
-        <Jumbotron>
-          <p className='lead'>Get an email when your game goes on sale.</p>
-          <TitleSearch {...this.props} />
-        </Jumbotron>
-
-        {this.props.allGames.length > 0 && (
-          <div>
-            <p className='lead' style={{ 'margin': '1em' }}>Currently tracking</p>
-            <GamesGrid allGames={this.props.allGames} />
-          </div>)}
+        <Row>
+          <Col>
+            <div style={{ maxWidth: '50em', margin: '0 auto 5em auto' }}>
+              <Jumbotron>
+                <p className='lead'>Get an email when your game goes on sale.</p>
+                <TitleSearch {...this.props} />
+              </Jumbotron>
+            </div>
+          </Col>
+        </Row>
 
         {this.props.popularGames.length > 0 && (
-          <div>
-            <p className='lead' style={{ 'margin': '1em' }}>Most popular</p>
-            <GamesGrid allGames={this.props.popularGames} />
-          </div>)}
+          <GamesGrid allGames={this.props.popularGames} />)}
       </div>
     );
   }
