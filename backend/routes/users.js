0da2a6662f954bfa1cc499c593fa70d368087608
Chris Vita <chris.vita@gmail.com>
2018-01-09 09:33:25 -0800

    Add `/users` routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,7 @@
 import Router from 'express-promise-router';
 import db from '../db/database';
+import { decrypt } from '../api/encrypt';
+import helper from './helper';
 const router = new Router();
 
 
@@ -14,8 +16,59 @@ router.get('/:email', async (req, res) => {
     }
     res.status(200).send(false);
   } catch (e) {
-    res.status(500).send(e.message);
+    helper.handleError(e, res);
   }
+});
+
+// Get user and price alert info
+router.get('/manage/:alert_id/:email', async (req, res) => {
+  const alert_id = parseInt(decrypt(req.params.alert_id));
+  const email = decrypt(req.params.email);
+  const queryText = (
+    `SELECT *
+      FROM users u
+      LEFT JOIN price_alerts a
+        ON u.email = a.email AND a.alert_id = '${alert_id}'
+      WHERE u.email = '${email}'`
+  );
+  try {
+    const request = await db.query(queryText);
+    if (request.rowCount > 0) {
+      const data = request.rows[0];
+      data.email = email;
+      res.status(200).send(data);
+    }
+  } catch (e) {
+    helper.handleError(e, res);
+  }
+});
+
+// Update user info to be on_blacklist, which automatically deletes all price alerts
+router.put('/', async (req, res) => {
+  const { email } = req.body;
+  try {
+    const blacklistUser = await db.query(`UPDATE users SET on_blacklist = 'true' WHERE email = '${email}'`);
+    if (blacklistUser.rowCount === 0) {
+      res.status(404).send(`Unable to find '${email}' in users, so no action was taken.`);
+      return;
+    }
+    const deletePriceAlerts = await db.query(`DELETE FROM price_alerts WHERE email = '${email}'`);
+    res.status(200).send(blacklistUser.rowCount > 0);
+  } catch (e) {
+    helper.handleError(e, res);
+  }
+});
+
+// Delete user
+router.delete('/:email', async (req, res) => {
+  const { email } = req.params;
+  try {
+    const request = await db.query(`DELETE FROM users WHERE email = '${email}'`);
+    res.status(200).send(request.rowCount === 1);
+  } catch (e) {
+    helper.handleError(e, res);
+  }
+
 });
 
 
