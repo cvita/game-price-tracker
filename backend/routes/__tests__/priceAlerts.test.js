0d9f41f4d1f9d040a03a5ee18d5c90f0ea6acb3d
Chris Vita <chris.vita@gmail.com>
2018-01-09 09:31:43 -0800

    Add tests for new routes
    Both routes in same file allows tests to behave more like acceptance test.

--- backend/routes/__tests__/priceAlerts.test.js
+++ backend/routes/__tests__/priceAlerts.test.js
@@ -1,4 +1,5 @@
 import request from 'supertest';
+import { encrypt } from '../../api/encrypt';
 import app from '../../app';
 
 
@@ -8,20 +9,63 @@ const example = {
   price: 19.99
 };
 
-
-describe('route: `/priceAlerts`', () => {
-  it('handles a POST request to `/priceAlerts` by upserting the example price alert, and creating a user', () => {
+// Testing both routes in same file enables unit tests to behave more like an acceptance test.
+describe('routes: `/priceAlerts`, `/users`', () => {
+  it('handles POST `/priceAlerts` by upserting a price alert, and creating the user', () => {
     return request(app)
       .post('/priceAlerts')
       .type('form')
       .send(example)
       .expect(200)
+      .then(res => {
+        const { priceAlertSaved, alert_id } = res.body;
+        expect(priceAlertSaved).toEqual(true);
+        expect(typeof alert_id).toBe('number');
+        example.alert_id = alert_id;
+      });
+  });
+
+  it('handles GET `/users/manage/:alert_id/:email` by getting user and price alert info', () => {
+    return request(app)
+      .get(`/users/manage/${encrypt(example.alert_id.toString())}/${encrypt(example.email)}`)
+      .expect(200)
+      .then(res => {
+        const { on_blacklist, email, game_id, price, alert_id } = res.body;
+        expect(on_blacklist).toEqual(false);
+        expect(email).toEqual(example.email);
+        expect(game_id).toEqual(example.gameId);
+        expect(price).toEqual(example.price);
+        expect(alert_id).toEqual(example.alert_id);
+      });
+  });
+
+  it('handles DELETE `/priceAlerts/:game_id/:email` by deleting the example price alert', () => {
+    return request(app)
+      .delete(`/priceAlerts/${example.gameId}/${encodeURIComponent(example.email)}`)
+      .expect(200)
       .then(res => expect(res.body).toEqual(true));
   });
 
-  it('handles a DELETE request to `/priceAlerts/:game_id/:email` by deleting the example price alert', () => {
+  it('handles PUT `/users` by adding user to blacklist and deleting all users\' price alerts', () => {
     return request(app)
-      .delete(`/priceAlerts/${example.gameId}/${encodeURIComponent(example.email)}`)
+      .put('/users')
+      .type('form')
+      .send({ email: example.email })
+      .expect(200)
+      .then(res => expect(res.body).toEqual(true));
+  });
+
+  it('handles GET `/users/:email` by checking user\' blacklist status', () => {
+    return request(app)
+      .get(`/users/${encodeURIComponent(example.email)}`)
+      .expect(200)
+      .then(res => expect(res.body).toEqual(true));
+  });
+
+  // This route is not currently used by the app, by may be later. Including now to tear down test.
+  it('handles DELETE `/users/:email` by deleting user', () => {
+    return request(app)
+      .delete(`/users/${encodeURIComponent(example.email)}`)
       .expect(200)
       .then(res => expect(res.body).toEqual(true));
   });
