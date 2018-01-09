7c503786033c134486ae45240539762dc4b7bbcb
Chris Vita <chris.vita@gmail.com>
2018-01-09 09:36:46 -0800

    Add new client methods

--- frontend/src/client/db.js
+++ frontend/src/client/db.js
@@ -16,6 +16,41 @@ const upsertPriceAlert = async info => {
   };
   try {
     const request = await fetch('/priceAlerts', init);
+    const response = await request.json();
+    return Promise.resolve(response.priceAlertSaved);
+  } catch (e) {
+    return Promise.reject(e);
+  }
+};
+
+const fetchPriceAlert = async (alertId, email) => {
+  const init = { method: 'GET' };
+  try {
+    const request = await fetch(`/users/manage/${alertId}/${email}`, init);
+    return Promise.resolve(await request.json());
+  } catch (e) {
+    return Promise.reject(e);
+  }
+};
+
+const deletePriceAlert = async (gameId, email) => {
+  const init = { method: 'DELETE' };
+  try {
+    const request = await fetch(`/priceAlerts/${gameId}/${email}`, init);
+    return Promise.resolve(await request.json());
+  } catch (e) {
+    return Promise.reject(e);
+  }
+};
+
+const addToBlacklist = async email => {
+  const init = {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email })
+  };
+  try {
+    const request = await fetch('/users', init);
     return Promise.resolve(await request.json());
   } catch (e) {
     return Promise.reject(e);
@@ -25,5 +60,8 @@ const upsertPriceAlert = async info => {
 
 export default {
   checkBlacklist,
-  upsertPriceAlert
+  upsertPriceAlert,
+  fetchPriceAlert,
+  deletePriceAlert,
+  addToBlacklist
 };
