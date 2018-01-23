77edc929c65fde51c40e27cb44f707678c3729a0
Chris Vita <chris.vita@gmail.com>
2018-01-23 08:04:28 -0800

    Fix issue #17
    Function expressions to prevent hoisting above `import...`.

--- backend/scheduled/schedule.js
+++ backend/scheduled/schedule.js
@@ -1,32 +1,11 @@
+import 'babel-polyfill';
 import db from '../db/database';
 import sony from '../support/querySony';
 import nodemail from '../support/email'
 import helper from '../routes/helper';
 
 
-// Called from Heroku Scheduler
-updateInfoAndInformUsers()
-  .then(status => console.log(status));
-
-function updateInfoAndInformUsers() {
-  return new Promise((resolve, reject) => {
-    const status = { started: new Date().getTime() };
-
-    Promise.all([handleExpiredAlerts(), handleCurrentAlerts()])
-      .then(results => {
-        status.expiredAlerts = results[0];
-        status.currentGames = results[1];
-        status.elapsed = new Date().getTime() - status.started;
-        resolve(status);
-      })
-      .catch(e => {
-        console.error(e);
-        reject(e);
-      });
-  });
-}
-
-async function handleExpiredAlerts() {
+const handleExpiredAlerts = async () => {
   try {
     const request = await db.query('DELETE FROM price_alerts WHERE expires <= now() RETURNING *');
     request.rows.map(priceAlert => nodemail.sendRemovingPriceAlert(priceAlert));
@@ -36,7 +15,7 @@ async function handleExpiredAlerts() {
   }
 };
 
-async function handleCurrentAlerts() {
+const handleCurrentAlerts = async () => {
   try {
     const request = await db.query('SELECT * FROM price_alerts WHERE expires > now()');
     const processes = request.rows.map(priceAlert => (
@@ -55,6 +34,28 @@ async function handleCurrentAlerts() {
     console.error(e);
   }
 };
+
+const updateInfoAndInformUsers = () => {
+  return new Promise((resolve, reject) => {
+    const status = { started: new Date().getTime() };
+
+    Promise.all([handleExpiredAlerts(), handleCurrentAlerts()])
+      .then(results => {
+        status.expiredAlerts = results[0];
+        status.currentGames = results[1];
+        status.elapsed = new Date().getTime() - status.started;
+        resolve(status);
+      })
+      .catch(e => {
+        console.error(e);
+        reject(e);
+      });
+  });
+};
+
+// Called from Heroku Scheduler
+updateInfoAndInformUsers()
+  .then(status => console.log(status));
 
 
 module.exports = {
